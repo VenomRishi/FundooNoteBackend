2c4e7162515cda5ec0014749cb595dd484a9ba59
VenomRishi <mhatrerishikesh@gmail.com>
2019-12-07 16:45:16 +0530

    zuul server implemented successfully but testing is pending

--- ZuulApiGateway/src/main/java/com/bridgelabz/apigateway/ZuulApiGatewayApplication.java
+++ ZuulApiGateway/src/main/java/com/bridgelabz/apigateway/ZuulApiGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.bridgelabz.apigateway.filter.SimpleFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +16,10 @@ public class ZuulApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewayApplication.class, args);
 	}
+	
+	@Bean
+	  public SimpleFilter simpleFilter() {
+	     return new SimpleFilter();
+	  }
 
 }
